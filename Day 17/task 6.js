a703e5873cfb52ab2066c0649f923b4f19e491c5
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-27 21:39:58 +0530

    Create task 6.js

---
+++ Day 17/task 6.js
@@ -0,0 +1,8 @@
+let name = "Sanjay"
+let age = 23
+let course = "Frontend"
+let isEnrolled = true;
+console.log("Student Name:"+ name);
+console.log("Age:"+ age);
+console.log("Course:"+ course);
+console.log("Enrolled:"+ isEnrolled);
