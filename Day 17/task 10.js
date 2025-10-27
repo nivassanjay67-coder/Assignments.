93332a8a2d8ddaca5bee9fe22a0d76d4bf438ca4
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-27 21:48:34 +0530

    Create task 10.js

---
+++ Day 17/task 10.js
@@ -0,0 +1,8 @@
+let message1 = "Age: " + 20;
+let message2 = "Total: " + (10 + 5);
+let message3 = 10 + 5 + " is the sum";
+let message4 = "The result is " + 10 + 5;
+console.log(message1);
+console.log(message2);
+console.log(message3);
+console.log(message4);
