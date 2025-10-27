492b2ab68969de62dbbf08675172040b5706e6fe
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-27 21:43:52 +0530

    Create task 7.js

---
+++ Day 17/task 7.js
@@ -0,0 +1,8 @@
+let studentName = "Alice Johnson";   
+let studentAge = 21;                 
+let course = "Computer Science";     
+let isEnrolled = true;               
+console.log("Student Name:", studentName, "| Type:", typeof studentName);
+console.log("Age:", studentAge, "| Type:", typeof studentAge);
+console.log("Course:", course, "| Type:", typeof course);
+console.log("Enrolled:", isEnrolled, "| Type:", typeof isEnrolled);
