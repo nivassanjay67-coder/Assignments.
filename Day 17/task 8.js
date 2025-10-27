7b5e74dc126a72f998630cc51e6461026fbe1976
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-27 21:45:30 +0530

    Create task 8.js

---
+++ Day 17/task 8.js
@@ -0,0 +1,8 @@
+let favoriteMovies = ["Inception", "Interstellar", "The Dark Knight", "Avengers: Endgame"];
+let laptop = {
+  brand: "Dell",
+  RAM: "16GB",
+  price: 85000  
+};
+console.log("Favorite Movies:", favoriteMovies);
+console.log("Laptop Details:", laptop);
