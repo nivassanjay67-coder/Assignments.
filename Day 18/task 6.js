d37721668b099a2285f8fb67687495d463e5e414
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-29 20:49:07 +0530

    Create task 6.js

---
+++ Day 18/task 6.js
@@ -0,0 +1,8 @@
+let age = 20
+if (age < 0) {
+    console.log("Please enter a valid age.");
+} else if (age >= 18) {
+    console.log("You can drive.");
+} else {
+    console.log("You cannot drive yet.");
+}
