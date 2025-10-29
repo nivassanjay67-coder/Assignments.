606651905e300ff8842373c6ea0caec850c51619
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-29 20:56:11 +0530

    Create task 8.js

---
+++ Day 18/task 8.js
@@ -0,0 +1,6 @@
+let password = 10
+if (password.length >= 8) {
+    console.log("Your password is secure.");
+} else {
+    console.log("Your password is not secure. It should be at least 8 characters long.");
+}
