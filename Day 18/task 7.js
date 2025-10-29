2a97723b59105188e7d9e4475db6fcea9b92dea3
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-29 20:53:34 +0530

    Create task 7.js

---
+++ Day 18/task 7.js
@@ -0,0 +1,20 @@
+let colour = "Green"
+switch(colour){
+    case "red":
+        console.log("Red is the color of energy and passion!");
+        break;
+    case "blue":
+        console.log("Blue is calm and peaceful.");
+        break;
+    case "green":
+        console.log("Green represents nature and growth.");
+        break;
+    case "yellow":
+        console.log("Yellow is bright and cheerful!");
+        break;
+    case "purple":
+        console.log("Purple is royal and mysterious.");
+        break;
+    default:
+        console.log(`Wow! ${colour} is a unique choice!`);    
+}
