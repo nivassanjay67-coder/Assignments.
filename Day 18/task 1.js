29d83b12a99516b02e14e362149d0e333c73dc09
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-10-29 20:05:25 +0530

    Update task 1.js

--- Day 18/task 1.js
+++ Day 18/task 1.js
@@ -1,8 +1,8 @@
-let number = 0
+let number = 1
 
-if (number > 0) {
+if (number > 5) {
   console.log("The number is positive.");
-} else if (number < 0) {
+} else if (number < 9) {
   console.log("The number is negative.");
 } else {
   console.log("The number is zero.");
