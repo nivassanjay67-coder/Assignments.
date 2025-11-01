c7f488c0395e4c0158c710175a8b8999283c2cd9
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-11-01 11:00:05 +0530

    Create task 2.js

---
+++ Day 21/task 2.js
@@ -0,0 +1,9 @@
+let colour = ["red","blue"]
+colour.push("black")
+console.log(colour);
+colour.pop()
+console.log(colour);
+colour.shift()
+console.log(colour);
+colour.unshift("pink")
+console.log(colour);
