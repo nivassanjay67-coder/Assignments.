c98afc782398bf5c1eed9f4c9299a2038e835317
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-11-03 17:14:34 +0530

    Update task 4.js

--- Day 22/task 4.js
+++ Day 22/task 4.js
@@ -8,5 +8,5 @@ class Employee {
     console.log(`Employee Name: ${this.name}, Salary: $${this.salary}`);
   }
 }
-const emp1 = new Employee("Alice Johnson", 55000);
+const emp1 = new Employee("Sanjay", 55000);
 emp1.displayInfo();
