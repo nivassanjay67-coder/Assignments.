aa28d110c4015441d473a643552a68ce4af4f8ac
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-11-03 17:13:57 +0530

    Create task 4.js

---
+++ Day 22/task 4.js
@@ -0,0 +1,12 @@
+class Employee {
+  constructor(name, salary) {
+    this.name = name;
+    this.salary = salary;
+  }
+
+  displayInfo() {
+    console.log(`Employee Name: ${this.name}, Salary: $${this.salary}`);
+  }
+}
+const emp1 = new Employee("Alice Johnson", 55000);
+emp1.displayInfo();
