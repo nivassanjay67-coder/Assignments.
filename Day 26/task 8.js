b915c55434b980782b2066e9356091ffb9ab3762
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-11-11 12:35:19 +0530

    Create task 8.js

---
+++ Day 26/task 8.js
@@ -0,0 +1,11 @@
+const student = {
+  name: "Alice",
+  age: 22,
+  course: "Computer Science"
+};
+
+const { name, age, course } = student;
+
+console.log(name);   
+console.log(age);    
+console.log(course); 
