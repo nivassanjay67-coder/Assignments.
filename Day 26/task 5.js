89e3c42f5b30b29f7c39a1430bc56d78003d921a
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-11-11 12:27:42 +0530

    Create task 5.js

---
+++ Day 26/task 5.js
@@ -0,0 +1,12 @@
+const user = {
+  name: "Alice",
+  age: 25,
+  city: "New York",
+  role: "Developer"
+};
+
+console.log(`User Details:
+Name: ${user.name}
+Age: ${user.age}
+City: ${user.city}
+Role: ${user.role}`);
