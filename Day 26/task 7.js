64034647f198bb7cc79a5f9b815139f33207a89b
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-11-11 12:31:26 +0530

    Create task 7.js

---
+++ Day 26/task 7.js
@@ -0,0 +1,5 @@
+const studentsGroupA = ["Alice", "Bob", "Charlie"];
+const studentsGroupB = ["David", "Emma", "Frank"];
+const allStudents = [...studentsGroupA, ...studentsGroupB];
+
+console.log(allStudents);
