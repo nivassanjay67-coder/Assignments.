87f5f119181a4a01b76cddd9f8e0ee42b59e0eca
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-11-20 19:02:12 +0530

    Create task 10.sql

---
+++ Day 32/task 10.sql
@@ -0,0 +1,17 @@
+CREATE TABLE products (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,  
+    name VARCHAR(100) NOT NULL,                
+    price DECIMAL(10,2) NOT NULL,             
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP  
+);
+INSERT INTO products (name, price)
+VALUES
+    ('Laptop', 1200.00),
+    ('Smartphone', 800.00),
+    ('Headphones', 150.00),
+    ('Keyboard', 75.00),
+    ('Monitor', 300.00);
+SELECT *
+FROM products
+ORDER BY price DESC
+LIMIT 3;
