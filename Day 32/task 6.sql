7f3fc7b8200bcc4031f760e8278e34b4db28158b
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-11-20 18:54:47 +0530

    Update task 6.sql

--- Day 32/task 6.sql
+++ Day 32/task 6.sql
@@ -1,6 +1,6 @@
 CREATE TABLE products (
     product_id INT PRIMARY KEY AUTO_INCREMENT,  
-    name VARCHAR(100) NOT NULL,                
-    price DECIMAL(10,2) NOT NULL,             
+    name VARCHAR(100),                
+    price DECIMAL(10,2),             
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP  
 );
