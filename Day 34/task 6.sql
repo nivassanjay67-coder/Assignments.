a515d4c130ed5ae227cf46a87133034b2fcfd5b7
nivassanjay67-coder <nivassanjay67@gmail.com>
2025-11-21 18:56:47 +0530

    Create task 6.sql

---
+++ Day 34/task 6.sql
@@ -0,0 +1,21 @@
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    salary DECIMAL(10,2),
+    hire_date DATE,
+    department_id INT,
+    active TINYINT
+);
+INSERT INTO employees (employee_id, first_name, last_name, salary, hire_date, department_id, active) VALUES
+(1, 'John', 'Doe', 60000, '2019-05-12', 1, 1),
+(2, 'Sarah', 'Miller', 82000, '2020-03-18', 2, 1),
+(3, 'David', 'Kim', 92000, '2021-07-25', 3, 1),
+(4, 'Emma', 'Stone', 74000, '2022-11-09', 5, 1),
+(5, 'Michael', 'Lee', 78000, '2023-02-14', 4, 1),
+(6, 'Sophia', 'Brown', 67000, '2021-01-10', 3, 1),
+(7, 'Lucas', 'White', 105000, '2020-06-05', 2, 1),
+(8, 'Ava', 'Green', 99000, '2018-11-20', 1, 1);
+SELECT department_id, COUNT(*) AS num_employees
+FROM employees
+GROUP BY department_id;
